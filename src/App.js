5201ac5c63a5bdbd67eb1f949741760434d3d87b
Ferdinand Lukito <devferdinandlukito@gmail.com>
2023-12-08 13:33:39 -0500

    third commit
    HomePage component rendered together on the root path via Nested Routes and Index Route technique

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ import Root from './components/root';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(createRoutesFromElements (
-  <Route path='/' element={<Root/>}/>
+  <Route path='/' element={<Root/>}>
+    <Route index element={<HomePage/>}></Route>
+  </Route>
+
 ));
 
 function App() {
