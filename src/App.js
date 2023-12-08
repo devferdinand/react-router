f9997033eb34d2c8efc787864503e02b9615091a
Ferdinand Lukito <devferdinandlukito@gmail.com>
2023-12-08 15:30:03 -0500

    sixth commit
    Imperatively redirect page via useNavigate hook to update URL to include search query input. Added Root child Route to render SearchPage when search query input occurs. Used useSearchParams hook in SearchPage to get the parameters from the URL to show the correct search results.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const appRouter = createBrowserRouter(createRoutesFromElements (
     <Route index element={<HomePage/>}></Route>
     <Route path=':type' element={<HomePage/>}></Route>
     <Route path='//:type/:id' element={<PetDetailsPage/>}></Route>
+    <Route path='search' element={<SearchPage/>}></Route>
   </Route>
 
 ));
